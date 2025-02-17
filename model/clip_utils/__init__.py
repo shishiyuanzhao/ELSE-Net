8a4f56305aac40ce5edd41731c732e48b91c921a
shishiyuanzhao <166681589+shishiyuanzhao@users.noreply.github.com>
2025-02-17 16:24:37 +0800

    Add files via upload

---
+++ model/clip_utils/__init__.py
@@ -0,0 +1,3 @@
+from .utils import get_predefined_templates
+from .visual import FeatureExtractor, RecWithAttnbiasHead,FeatureExtractor_No
+from .classifier import PredefinedOvClassifier, LearnableBgOvClassifier
